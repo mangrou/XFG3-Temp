2ae7ea1c57f0cbfb49369f534e7351985fb60951
zbonakle <lenka.zbonakova@hu-berlin.de>
2016-09-01 16:49:06 +0200

    Update XFGLambdaVIX

--- XFGLambdaVIX/XFGLambdaVIX.R
+++ XFGLambdaVIX/XFGLambdaVIX.R
@@ -1,22 +1,40 @@
-setwd("")
-tmpdata1 = read.csv("lambda_mean_206vars_2016-07-15.csv", sep=",")
-tmpdata2 = read.csv("200_firms_returns_and_scaled_macro_2016-08-18.csv", sep=",")
+# Clear all variables
+rm(list = ls(all = TRUE))
+graphics.off()
 
-lambda       = tmpdata1[, 2]
-dates        = tmpdata1[, 1]
-vix          = tmpdata2[128:2400, 202]
-vix.dates    = tmpdata2[128:2400, 1]
+# Set working directory
+# setwd("")
 
+# Install and load packages
+libraries = c("quantmod")
+lapply(libraries, function(x) if (!(x %in% installed.packages())) {
+  install.packages(x)} )
+lapply(libraries, library, quietly = TRUE, character.only = TRUE)
+
+# Read the file with time series of average lambda from FRM
+tmpdata = read.csv("lambda_mean_206vars_2016-07-15.csv", sep=",")
+
+# Create vector of (normalized) lambda values and dates
+lambda      = tmpdata[, 2]
+lambda.norm = (lambda - min(lambda))/(max(lambda) - min(lambda))
+dates       = as.Date(tmpdata[, 1], "%Y-%m-%d")
+
+# Download daily VIX values from Yahoo Finance and normalize
+start = dates[1]
+end   = dates[length(lambda)]
+vix   = as.vector(getSymbols("^VIX", src = "yahoo", from = start, to = end, 
+                             auto.assign = FALSE)[, 6])
+vix.norm = (vix - min(vix))/(max(vix) - min(vix))
+
+# Plot normalized time series of FRM lambda and VIX
 par(mfrow = c(1,1))
-vix.norm     = (vix - min(vix))/(max(vix) - min(vix))
-lambda.norm  = (lambda - min(lambda))/(max(lambda) - min(lambda))
 par(mar = c(5, 6, 1, 1))
 plot(vix.norm, type = "l",  col =  "darkblue", axes = FALSE, 
-     xlab = "Year", frame = TRUE, cex.main = 1.5, ylab = expression(paste("Average ", lambda)),
-     cex.lab = 2)
-at.tmp = c(grep("2008", dates)[1], grep("2009", dates)[1], grep("2010", dates)[1], grep("2011", dates)[1], 
-           grep("2012", dates)[1], grep("2013", dates)[1], grep("2014", dates)[1], grep("2015", dates)[1],
-           grep("2016", dates)[1])
+     xlab = "Year", frame = TRUE, cex.main = 1.5, 
+     ylab = expression(paste(lambda, " vs. VIX")), cex.lab = 2)
+at.tmp = c(grep("2008", dates)[1], grep("2009", dates)[1], grep("2010", dates)[1], 
+           grep("2011", dates)[1], grep("2012", dates)[1], grep("2013", dates)[1], 
+           grep("2014", dates)[1], grep("2015", dates)[1], grep("2016", dates)[1])
 axis(1, cex.axis = 1.5, labels = c(2008:2016), at = at.tmp)
 axis(2, cex.axis = 1.5)
 lines(lambda.norm, col = "red3")
